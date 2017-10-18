0d38d0f3126b3c9fc9ad420368775fb53a337ce1
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-10-18 07:25:32 +0000

    Get to compile with gcc5.x

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <memory>
 #include "checkpoints.h"
 #include "db.h"
 #include "net.h"
